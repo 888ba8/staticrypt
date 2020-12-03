a747d8e219044128e9972a6604c48b17cfc9dc04
888ba8 <75320002+888ba8@users.noreply.github.com>
2020-12-03 21:30:08 +0100

    Update index.js

--- cli/index.js
+++ cli/index.js
@@ -29,7 +29,7 @@ const namedArgs = Yargs
           alias: 'title',
           type: 'string',
           describe: "Title for output HTML page",
-          default: 'Protected Page'
+          default: 'Protected Website'
       })
       .option('i', {
           alias: 'instructions',
